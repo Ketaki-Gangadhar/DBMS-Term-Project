cdece00de8cefd19d2ec33c23da59cb80327d087
Ketaki-Gangadhar <ketakigangadhar0811@gmail.com>
2021-04-02 19:04:21 +0530

    dbms project

---
+++ app/src/main/java/ketaki/mycompany/hostelmanagementsystem/HomeActivity.java
@@ -0,0 +1,32 @@
+package ketaki.mycompany.hostelmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+private Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();
+
+        b1 = findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        });
+    }
+
+    private void start() {
+        Intent i = new Intent(HomeActivity.this,Form.class);
+        startActivity(i);
+        finish();
+    }
+}
